99bfa25921d105e671bf8b89f834a6860f9c098a
Chris <orj.tsuk@gmail.com>
2021-08-01 17:43:00 -0400

    Getting the modules correctly set up, and learning how to expose what I need to get the game to run.

--- script.js
+++ script.js
@@ -6,18 +6,105 @@ const GAMEBOARD = (function(){
     ];
 
     const boardSetup = () =>{
-        let tiles = document.querySelectorAll('.item')
+        const tiles = document.querySelectorAll('.item')
         tiles.forEach(
             function(item){
                 item.addEventListener("click", (e)=>{
-                    if(e.target != document.body)
-                    e.target.textContent = 'X'
+                    if(e.target != document.body){
+                        turn = PLAYERS.playerTurn()
+                        e.target.textContent = turn
+                        PLAYERS.setPlayerSymbols()
+                    }
+
                 })
         })
     }
 
+    const boardReset = () => {
+        const tiles = document.querySelectorAll('.item')
+        tiles.forEach (
+            function(item){
+                item.textContent = ""
+            }
+        )
+    }
+
     return{
-        boardSetup
+        boardSetup,
+        boardReset,
+    }
+})();
+
+
+const PLAYERS = (function(){ 
+    _players = [];
+    const symbolArray = ["X", "O"]
+
+    const createPlayer = () =>{
+        pName = document.querySelector('.playerInput').value
+        symbol = symbolArray.pop()
+        myTurn = Math.floor(Math.random() * 2)
+        switch (myTurn){
+            case 0:
+                myTurn = false
+                break
+            case 1:
+                myTurn = true
+                break
+        }
+        player = {pName, symbol, myTurn}
+        if(_players.length == 1){
+            if(_players[0].myTurn = true)
+            player.myTurn = false
+        }else{
+            player.myTurn = true
+        }
+        if(_players.length < 2){
+            _players.push(player)
+        }
+        document.querySelector('.playerInput').value = ''
+    }
+
+
+    function playerTurn(){
+        for(i = 0; i < _players.length; i++){
+            if(_players[i].myTurn == true){
+                return _players[i].symbol
+            }
+        }
+    }
+
+    function getPlayerSymbol(playerId){
+        for(i = 0; i < _players.length; i++){
+            if(i == playerId){
+                return _players[i].symbol
+            }
+        }
+    }
+
+    function setPlayerSymbols(){
+        for(i = 0; i < _players.length;i++){
+            if(_players[i].myTurn == true){
+                _players[i].myTurn = false
+            }else{
+                _players[i].myTurn = true
+            }
+        }
+    }
+
+    function getPlayerTurn(playerId){
+        for(i = 0; i < _players.length; i++){
+            if(i == playerId){
+                return _players[i].myTurn
+            }
+        }
+    }
+    return{
+        createPlayer,
+        getPlayerSymbol,
+        getPlayerTurn,
+        playerTurn,
+        setPlayerSymbols
     }
 })();
 
